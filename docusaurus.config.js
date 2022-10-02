fc307c7100f965f87698627f7ca349d669bdd559
jithinsankar <jithinsankar.nk@gmail.com>
2022-10-02 17:08:02 +0530

    modified config /blog to /root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://github.com',
-  baseUrl: '/blog/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
